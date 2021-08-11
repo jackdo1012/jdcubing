8d84c7440da9992bc74107226302c3d09dc5b3d4
jackdo1012 <dominhtrijack@gmail.com>
2021-08-11 16:35:28 +0700

    fix some more

--- src/components/Penalty.js
+++ src/components/Penalty.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getFirstMiniStats } from "../reducers/getMiniStats/getFirstMiniStat"
 import { getSecondMiniStats } from "../reducers/getMiniStats/getSecondMiniStat"
